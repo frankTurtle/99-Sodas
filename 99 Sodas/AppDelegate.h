48c75df1017ba65b7ae3983ccba35af7cd54f0e8
Barret J. Nobel <bear.nobel@gmail.com>
2015-02-22 10:13:37 -0500

    Initial Commit

---
+++ 99 Sodas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  99 Sodas
+//
+//  Created by OSX on 2/22/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 99 Sodas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  99 Sodas
+//
+//  Created by OSX on 2/22/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
